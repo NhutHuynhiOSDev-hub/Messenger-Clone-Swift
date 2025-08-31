f8b8a4d7026784253f6fbd24cc6b2c217f6c96d2
nhuthq <nhuthq@zigexn.vn>
2025-08-31 12:02:50 +0700

    init project

---
+++ MessengerClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerClone
+//
+//  Created by Nhut Huynh Quang on 31/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
